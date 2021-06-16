e44cbae677a87671d277a29ed6c3d14880100b9e
StefanPc <stefanivic93@outlook.com>
2021-06-16 23:53:51 +0200

    commit to master

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:flutter_basics/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app is trigger a frame.
+    // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
